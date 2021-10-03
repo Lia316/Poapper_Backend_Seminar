95eb5946794134122852bf61c5e472b88ec6e9e7
Lia <kjk0825@postech.ac.kr>
2021-10-03 22:59:38 +0900

    feat✨: solve Node.js tutorial problem

---
+++ week1/prob2.js
@@ -0,0 +1,16 @@
+function fibo(n){
+    let f0 = 0, f1 = 1, result;
+
+    if(n<2) return n;
+
+    for(let i=2; i<=n; i++) {
+        result = f0 + f1;
+        f0 = f1;
+        f1 = result;
+    }
+    return result;
+}
+
+for(let i=0; i<18; i++) {
+    console.log(fibo(i));
+}
