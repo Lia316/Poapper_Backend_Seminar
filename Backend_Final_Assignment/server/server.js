033ef035dcbfa25b4b87c5b4bbeca95b8c386a38
Lia <kjk0825@postech.ac.kr>
2022-01-16 18:45:03 +0900

    fix: 🐞 base url에서 불필요한 get 코드 삭제

--- Backend_Final_Assignment/server/server.js
+++ Backend_Final_Assignment/server/server.js
@@ -6,10 +6,6 @@ const app = express()
 app.use(express.json())
 app.use("/study", studyRouter)
 
-app.get('', (req, res) => {
-    res.send("Main Page")
-})
-
 app.listen(port, () => {
     console.log(`server is running on ${port} port.`)
 })
